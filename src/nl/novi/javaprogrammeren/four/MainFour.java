dce54a29d9ed668d69dda38cdb0f1141f52f8fa8
Martijn Doensen <martijn.doensen@live.nl>
2021-01-12 13:01:23 +0100

    Finish all assignments

--- src/nl/novi/javaprogrammeren/four/MainFour.java
+++ src/nl/novi/javaprogrammeren/four/MainFour.java
@@ -1,8 +1,12 @@
 package nl.novi.javaprogrammeren.four;
 
+import nl.novi.javaprogrammeren.four.hidden.HumanMaker;
+
 public class MainFour {
     public static void main(String[] args) {
 
+        HumanMaker.makeHuman("Adam");
+        System.out.println(HumanMaker.getHumanName());
 
     }
 }
--- src/nl/novi/javaprogrammeren/four/hidden/HumanMaker.java
+++ src/nl/novi/javaprogrammeren/four/hidden/HumanMaker.java
@@ -2,6 +2,16 @@ package nl.novi.javaprogrammeren.four.hidden;
 
 public class HumanMaker {
 
+    private static Human human;
+
     private HumanMaker() {
+    }
+
+    public static Human makeHuman(String name) {
+        return human = new Human(name);
+    }
+
+    public static String getHumanName() {
+       return human.getName();
     }
 }
